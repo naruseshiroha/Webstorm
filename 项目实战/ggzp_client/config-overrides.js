2bba828a557802a7fc4c626f616f404d46ab046e
lixunsam <lixunsam@gmail.com>
2019-12-15 00:01:22 +0900

    ggzp_client

--- 项目实战/ggzp_client/config-overrides.js
+++ 项目实战/ggzp_client/config-overrides.js
@@ -1,19 +1,9 @@
-const {
-    override,
-    fixBabelImports,
-} = require("customize-cra");
-
+const {override, fixBabelImports, addLessLoader} = require("customize-cra")
 
 module.exports = override(
-
-    /*addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: { "@brand-primary": "#1cae82", "@brand-primary-tap": "#1DA57A", "@hd": "1px"},
-        //   strictMath: true,
-        //   noIeCompat: true,
-        //   localIdentName: '[local]--[hash:base64:5]'
-    }),*/
     fixBabelImports("import", {
-        libraryName: "antd-mobile", libraryDirectory: "es", style: "css" // change importing css to less
-    })
+        libraryName: "antd-mobile",
+        libraryDirectory: "es",
+        style: "css" // change importing css to less
+    }),
 )
--- 项目实战/ggzp_client/src/index.js
+++ 项目实战/ggzp_client/src/index.js
@@ -2,9 +2,7 @@
 * 入口js
 * */
 import React from "react";
-import ReactDOM from "react-dom";
+import {render} from "react-dom";
 import {Button} from "antd-mobile";
 
-// import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
-
-ReactDOM.render(<Button type="primary">Test</Button>, document.getElementById("root"))
+render(<Button type="primary">Test</Button>, document.getElementById("root"))
