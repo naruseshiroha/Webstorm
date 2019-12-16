fb6ca8b30c8e29276f674db92cc9fd902a6a3cb9
lixunsam <lixunsam@gmail.com>
2019-12-16 15:15:46 +0900

    unhandle andt diy

--- 项目实战/sgg_zp_client/src/containers/register/register.jsx
+++ 项目实战/sgg_zp_client/src/containers/register/register.jsx
@@ -14,7 +14,7 @@ import {
 
 import Logo from "../../components/logo/logo";
 
-const listItem = List.Item
+const ListItem = List.Item
 
 class Register extends Component {
     render() {
@@ -22,7 +22,27 @@ class Register extends Component {
             <div>
                 <NavBar>硅&nbsp;谷&nbsp;直&nbsp;聘</NavBar>
                 <Logo/>
-
+                <WingBlank>
+                    <List>
+                        <WhiteSpace/>
+                        <InputItem>用户名:</InputItem>
+                        <WhiteSpace/>
+                        <InputItem type="password">密&nbsp;&nbsp;&nbsp;码:</InputItem>
+                        <WhiteSpace/>
+                        <InputItem type="password">确认密码:</InputItem>
+                        <WhiteSpace/>
+                        <ListItem>
+                            <span>用户类型:</span>
+                            &nbsp;&nbsp;&nbsp;
+                            <Radio>大神</Radio>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio>老板</Radio>
+                        </ListItem>
+                        <WhiteSpace/>
+                        <Button type="primary">注册</Button>
+                        <Button>已有账户</Button>
+                    </List>
+                </WingBlank>
             </div>
         );
     }
