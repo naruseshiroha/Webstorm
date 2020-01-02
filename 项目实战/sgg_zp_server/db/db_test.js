17950cce57811655255c8d46c65c396edf1c4d88
lixunsam <lixunsam@gmail.com>
2020-01-02 16:32:15 +0900

    sgg_zp_server

--- 项目实战/sgg_zp_server/db/db_test.js
+++ 项目实战/sgg_zp_server/db/db_test.js
@@ -14,44 +14,54 @@
     3.4 通过 Model 的 remove()删除匹配的数据
 */
 
-const md5 = require("blueimp-md5")
+const md5 = require("blueimp-md5") // md5加密的函数
 
 // 1. 连接数据库
 // 1.1 引入 mongoose
 const mongoose = require("mongoose")
 // 1.2 连接指定数据库(URL 只有数据库是变化的)
-mongoose.connect("mongodb://localhost:27017/sgg_zp_test")
+mongoose.connect("mongodb://localhost:27017/sgg_zp_test", {useNewUrlParser: true, useUnifiedTopology: true})
 // 1.3 获取连接对象
 const conn = mongoose.connection
 // 1.4 绑定连接完成的监听(用来提示连接成功)
-conn.on("connected", () => {
+conn.once("open", () => { // 连接成功回调
     console.log("数据库连接成功, yeah!")
 })
 
 // 2. 得到对应特定集合的model
 // 2.1 定义Schema(描述文档结构)
-const userSchema = mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    type: {type: String, required: true}
+const userSchema = mongoose.Schema({ // 指定文档的结构: 属性名/属性值的类型, 是否是必须的, 默认值
+    username: { // 用户名
+        type: String,
+        required: true
+    },
+    password: { // 密码
+        type: String,
+        required: true
+    },
+    type: { // 用户类型
+        type: String,
+        required: true
+    },
+    header: {type: String} // 头像
 })
-// 2.2 定义Model(与集合对应, 可以操作集合)
-const UserModel = mongoose.model("user", userSchema) // 集合: users
-// CRUD
 
+// 2.2 定义Model(与集合对应, 可以操作集合)
+const UserModel = mongoose.model('user', userSchema) // 集合的名称为: users
+
+// 3. 通过Model或其实例对集合数据进行CRUD操作
 // 3.1 通过Model实例的save()添加数据
 function testSave() {
-    // user数据对象
     const user = {
-        username: "xfzhang",
-        password: md5("1234"),
-        type: "dashen"
+        username: 'Jack',
+        password: md5('456'),
+        type: 'laoban'
     }
-
+    // 创建UserModel实例
     const userModel = new UserModel(user)
-    // 保存到数据
-    userModel.save((err, user) => {
-        console.log("save", err, user)
+    // 调用save()保存
+    userModel.save((error, userDoc) => {
+        console.log('save()', error, userDoc)
     })
 }
 
@@ -59,16 +69,13 @@ function testSave() {
 
 // 3.2 通过Model的find()/findOne()查询多个或一个数据
 function testFind() {
-    // 查找多个
-    UserModel.find((err, user) => {
-        // 如果有匹配返回的是一个[user, user...] 集合
-        // 如果没有一个匹配的返回[]
-        console.log("find()", err, user)
+    // 查询多个: // 得到的是包含所有匹配文档对象的数组, 如果灭有匹配的就是[]
+    UserModel.find({_id: '5df82600dead1e397cca06ef'}, (error, users) => {
+        console.log('find()', error, users)
     })
-    // 查找一个
-    UserModel.findOne({_id: "5df82600dead1e397cca06ef"}, (err, user) => {
-        // 如果有匹配返回的是一个user, 如果没有一个匹配的返回null
-        console.log("findOne()", err, user)
+    // 查询一个: // 得到的是匹配的文档对象, 如果没有匹配的就是null
+    UserModel.findOne({_id: '5df82600dead1e397cca06ef'}, (error, user) => {
+        console.log('findOne()', error, user)
     })
 }
 
@@ -76,9 +83,19 @@ function testFind() {
 
 // 3.3 通过Model的findByIdAndUpdate()更新某个数据
 function testUpdate() {
-    UserModel.findByIdAndUpdate({_id:"5df82600dead1e397cca06ef"},{username:"xiaofz"}, (err,user) => {
-        console.log("findByIdAndUpdate()", err, user)
+    UserModel.findByIdAndUpdate({_id: '5e0c9de1b39c2436449e2f22'}, {username: 'Alice'}, (error, oldUser) => {
+        console.log('findByIdAndUpdate()', error, oldUser)
     })
 }
 
 // testUpdate()
+
+// 3.4 通过Model的remove()删除匹配的数据
+// { n: 1, ok: 1, deletedCount: 1 } // n表示删除数量  ok表示操作是否成功
+function testDelete() {
+    UserModel.remove({_id: '5df82600dead1e397cca06ef'}, (error, doc) => {
+        console.log('remove()', error, doc)
+    })
+}
+
+testDelete()
--- 项目实战/sgg_zp_server/routes/index.js
+++ 项目实战/sgg_zp_server/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const md5 = require('blueimp-md5')
+
+const {UserModel} = require("../db/models")
+const filter = {password: 0, __v: 0} // 指定查询时过滤出指定的属性
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+  res.render('index', {title: 'Express'});
 });
 
 // 注册一个路由: 用户注册
@@ -20,20 +25,43 @@ router.get('/', function (req, res, next) {
 * 2. 处理
 * 3. 返回x响应数据
 * */
-router.post("/register", function (req, res) { // req, res [,next]
-    console.log("register()")
-    // 1. 获取请求参数
-    const {username, password} = req.body
-    // 2. 处理
-    if (username === "admin") { // 注册会失败
-        // 返回响应数据(失败)
-        res.send({code: 1, message: "此用户已存在2333"})
-    } else { // 注册会失败
-        // 返回x响应数据(成功)
-        res.send({
-            code: 0, data: {id: "abc123", username, password}
-        })
+
+// 注册的路由
+router.post('/register', (req, res) => {
+  // 读取请求的参数数据
+  const {username, password, type} = req.body
+  // 处理: 判断用户是否已经存在, 如果存在, 提示返回错误的信息, 如果不存在, 保存
+  // 查询(根据 username )
+  UserModel.findOne({username}, (error, user) => {
+    // 如果 user 有值(已存在)
+    if (user) {
+      // 返回提示错误的信息
+      res.send({code: 1, msg: '此用户名已存在'})
+    } else { // user === null (不存在)
+      // 保存
+      new UserModel({username, type, password: md5(password)}).save((error, user) => {
+        // 返回包含 user 的 json 数据
+        const data = {username, type, _id: user._id} // 响应数据中不要携带 password
+        res.send({code: 0, data})
+      })
     }
+  })
+})
+
+// 登陆的路由
+router.post('/login', (req, res) => {
+  const {username, password} = req.body
+  // 根据 username 和 password 查询数据库 users, 如果没有, 返回提示错误的信息, 如果有, 返回登陆成功信息(包含user)
+  UserModel.findOne({username, password: md5(password)}, filter, (error, user) => {
+    if (user) { // 登录成功
+      // 生成一个cookie(userid: user_id), 并交给浏览器保存
+      res.cookie('userid', user._id, {maxAge: 1000 * 60 * 60 * 24 * 7})
+      // 返回登录成功信息(包含 user)
+      res.send({code: 0, data: user})
+    } else { // 登陆失败
+      res.send({code: 1, msg: '用户名或密码不正确'})
+    }
+  })
 })
 
 module.exports = router;
